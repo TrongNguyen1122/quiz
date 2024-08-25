532b24335841e1095bfa138ff2b7561cd80671c9
TrongNguyen <nguyenluu2723@gmail.com>
2024-08-25 19:59:31 +0700

    add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.scss'
-
+import Header from './components/Header/Header'
 function App() {
   return (
     <div className="App">
-      <h1>Hello word</h1>
+      <Header />
     </div>
   );
 }
